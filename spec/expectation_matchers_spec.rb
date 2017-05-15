db8134e816570fe37302bf2382825ce2b495562f
Courtland Messam <messamcourtland@gmail.com>
2017-05-14 21:38:41 -0400

    added truthiness matchers to espectation matchers file

--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -12,7 +12,7 @@ describe 'Expectation Matchers' do
       d = 17.0
       expect(c).to eq(d)        # different types, but "close enough"
     end
-    
+
     it 'will match value equality with #eql' do
       a = "2 cats"
       b = "2 cats"
@@ -32,7 +32,37 @@ describe 'Expectation Matchers' do
       expect(b).to equal(c)     # same object
       expect(b).to be(c)        # synonym for #equal
     end
-    
+
+  end
+
+  describe 'truthiness matchers' do
+
+    it 'will match true/false' do
+      expect(1 < 2).to be(true)       # do not use 'be_true'
+      expect(1 > 2).to be(false)      # do not use 'be_false'
+
+      expect('foo').not_to be(true)   # the string is not exactly true
+      expect(nil).not_to be(false)    # nil is not exactly false
+      expect(0).not_to be(false)      # 0 is not exactly false
+    end
+
+    it 'will match truthy/falsey' do
+      expect(1 < 2).to be_truthy
+      expect(1 > 2).to be_falsey
+
+      expect('foo').to be_truthy      # any value counts as true
+      expect(nil).to be_falsey        # nil counts as false
+      expect(0).not_to be_falsey      # but 0 is still not falsey enough
+    end
+
+    it 'will match nil' do
+      expect(nil).to be_nil
+      expect(nil).to be(nil)          # either way works
+
+      expect(false).not_to be_nil     # nil only, just like #nil?
+      expect(0).not_to be_nil         # nil only, just like #nil?
+    end
+
   end
 
 
